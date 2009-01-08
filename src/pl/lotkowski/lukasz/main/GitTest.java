0fd475fa20a9a27a4d65aa1ea546f2f95b5494a0
Lukasz Lotkowski <lotkowski.lukasz@gmail.com>
2009-01-08 15:00:48 +0100

    pierwsza wrzutka

---
+++ src/pl/lotkowski/lukasz/main/GitTest.java
@@ -0,0 +1,13 @@
+package pl.lotkowski.lukasz.main;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
